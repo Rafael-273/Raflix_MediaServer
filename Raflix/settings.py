8b193c1a13eb35c931d30f6ea36e4f3d3011c278
Rafael <rafaelpinheiro2273@gmail.com>
2022-12-13 16:16:18 -0300

    models_raflix

--- Raflix/settings.py
+++ Raflix/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'media',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
+    'embed_video',
     'debug_toolbar',
 ]
 
