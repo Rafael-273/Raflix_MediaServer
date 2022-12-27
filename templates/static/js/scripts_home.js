f9d8f280ffbae00136911528342c6a24015c298a
Rafael <rafaelpinheiro2273@gmail.com>
2022-12-27 00:43:51 -0300

    front

---
+++ templates/static/js/scripts_home.js
@@ -0,0 +1,151 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+});
+
+new Glider(document.querySelector('.glider1'), {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+});
+
+var link = document.getElementById('link')
+
+link.addEventListener("mouseover", function() {
+    link.play();
+})
+
+var link1 = document.getElementById('link1')
+
+
+link1.addEventListener("mouseover", function() {
+    link1.play();
+})
+
+var link2 = document.getElementById('link2')
+
+
+link2.addEventListener("mouseover", function() {
+    link2.play();
+})
+
+
+var link3 = document.getElementById('link3')
+
+
+link3.addEventListener("mouseover", function() {
+    link3.play();
+})
+
+var link4 = document.getElementById('link4')
+
+
+link4.addEventListener("mouseover", function() {
+    link4.play();
+})
+
+var link5 = document.getElementById('link5')
+
+
+link5.addEventListener("mouseover", function() {
+    link5.play();
+})
+
+var link6 = document.getElementById('link6')
+
+
+link6.addEventListener("mouseover", function() {
+    link6.play();
+})
+
+var link7 = document.getElementById('link7')
+
+
+link7.addEventListener("mouseover", function() {
+    link7.play();
+})
+
+var link8 = document.getElementById('link8')
+
+
+link8.addEventListener("mouseover", function() {
+    link8.play();
+})
+
+var link9 = document.getElementById('link9')
+
+
+link9.addEventListener("mouseover", function() {
+    link9.play();
+})
+
+var link10 = document.getElementById('link10')
+
+
+link10.addEventListener("mouseover", function() {
+    link10.play();
+})
+
+var link11 = document.getElementById('link11')
+
+
+link11.addEventListener("mouseover", function() {
+    link11.play();
+})
+
+var link12 = document.getElementById('link12')
+
+
+link12.addEventListener("mouseover", function() {
+    link12.play();
+})
+
+var link13 = document.getElementById('link13')
+
+
+link13.addEventListener("mouseover", function() {
+    link13.play();
+})
+
+var link14 = document.getElementById('link14')
+
+
+link14.addEventListener("mouseover", function() {
+    link14.play();
+})
+
+var link15 = document.getElementById('link15')
+
+
+link15.addEventListener("mouseover", function() {
+    link15.play();
+})
+
+var link16 = document.getElementById('link16')
+
+
+link16.addEventListener("mouseover", function() {
+    link16.play();
+})
+
+var link17 = document.getElementById('link17')
+
+
+link17.addEventListener("mouseover", function() {
+    link17.play();
+})
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+    // do something…
+  })
