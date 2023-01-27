aac5cc7fac4107b614c753f70516c81fd396d468
Rafael <rafaelpinheiro2273@gmail.com>
2023-01-27 14:03:16 -0300

    login_css

--- media/urls.py
+++ media/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('/login', views.Login.as_view(), name='login'),
-    path('/movies', views.Movies.as_view(), name='movies'),
-    path('/series', views.Series.as_view(), name='series'),
+    path('login', views.Login.as_view(), name='login'),
+    path('movies', views.Movies.as_view(), name='movies'),
+    path('series', views.Series.as_view(), name='series'),
     path('<slug>', views.Media.as_view(), name='media'),
-    path('/user', views.User.as_view(), name='user'),
-    path('/favorites', views.Favorites.as_view(), name='favorites'),
+    path('user', views.User.as_view(), name='user'),
+    path('favorites', views.Favorites.as_view(), name='favorites'),
 ]
