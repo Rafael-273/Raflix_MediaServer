89f8b6f22b21bbcd5f67bc436cd7172e98a72d7b
Rafael <rafaelpinheiro2273@gmail.com>
2023-04-24 20:40:22 -0300

    implementation postgresql

--- Raflix/settings.py
+++ Raflix/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Raflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'raflix_database',
+        'USER': 'rafael',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
